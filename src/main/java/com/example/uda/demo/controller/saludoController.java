7811af5254cb90dbc7caf05682cd61569e5e3955
Memo10100 <69870150+Memo10100@users.noreply.github.com>
2023-04-21 18:35:24 -0300

    Tercer Commit

--- src/main/java/com/example/uda/demo/controller/saludoController.java
+++ src/main/java/com/example/uda/demo/controller/saludoController.java
@@ -1,7 +1,6 @@
-package controller;
+package com.example.uda.demo.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class saludoController {
     @GetMapping("/saludar")
-    public ResponseEntity<?> saludar(){
+    public ResponseEntity<String> saludar(){
         try {
             return ResponseEntity.status(HttpStatus.OK).body("Hola Mundo");
         }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("no encontrado");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 }
